118f315c9019251cfb39b1b2205777f9e0698f7b
BlankaG <blankacamb@hotmail.com>
2020-10-13 16:05:59 +0000

    Database connected

--- run.py
+++ run.py
@@ -2,19 +2,20 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from pymongo import database
+
 
 app = Flask(__name__)
 
-#app.config["MONGO_DBNAME"] = 'Recipes'
+app.config["MONGO_DBNAME"] = 'Recipes'
 app.config["MONGO_URI"] = "mongodb+srv://blanka:blanka@cluster0.w783e.mongodb.net/Recipes?retryWrites=true&w=majority"
 
 mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/get_recipes')
-def get_recipes():
+def get_recipes(): 
     return render_template("about.html", recipes=mongo.db.recipes.find())
+    
 
 
 
